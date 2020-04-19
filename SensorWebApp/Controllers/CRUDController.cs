1bd3d928305a4bf577fc35e2374e44b3257d2d1e
Abrahim <abrahimdjordjevic@gmail.com>
2020-04-19 02:04:57 +0100

    First commit

---
+++ SensorWebApp/Controllers/CRUDController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using Microsoft.AspNetCore.Mvc;
+using SensorWebApp.Models;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SensorWebApp.Controllers
+{
+    public class CRUDController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                CRUDModel model = new CRUDModel();
+                SerialPort serialPort = new SerialPort("COM5", 9600);
+                SQLiteConnection conn = model.CreateConnection();
+                serialPort.Open();
+                Thread.Sleep(200);
+                String Data = serialPort.ReadExisting();
+                if (Data != "" && Data != "\n")
+                {
+                    model.InsertData(conn, Data);
+                }
+                DataTable dt = model.ReadData(conn);
+                serialPort.Close();
+                Console.WriteLine(Data);
+                return View("Index", dt);
+            }
+            catch (Exception e)
+            {
+                CRUDModel model = new CRUDModel();
+                SQLiteConnection conn = model.CreateConnection();
+                DataTable dt = model.ReadData(conn);
+                return View("Index", dt);
+
+            }
+        }
+    }
+}
---
+++ SensorWebApp/Models/CRUDModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SensorWebApp.Models
+{
+    public class CRUDModel
+    {
+       
+        public SQLiteConnection CreateConnection()
+            {
+                SQLiteConnection conn;
+                //create a new database connection
+                conn = new SQLiteConnection("Data Source = table.sqlite;");
+                //open the connection
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+                return conn;
+            }
+
+            public void createTable(SQLiteConnection conn)
+            {
+                SQLiteCommand SQLcmd = conn.CreateCommand();
+                SQLcmd.CommandText = "DROP TABLE Data; CREATE TABLE Data(Distance INT, Time VarChar(30));";
+            }
+      
+             public void InsertData(SQLiteConnection conn, String Data)
+            {
+                SQLiteCommand SQLcmd = conn.CreateCommand();
+                Data = Data.Replace("\r\n" , "");
+                String time = DateTime.Now.ToString("t");
+                time = time.Replace(":", ".");
+                SQLcmd.CommandText = "INSERT INTO Data(Distance, Time) VALUES(" + Data +","+ time +");";
+                SQLcmd.ExecuteNonQuery();
+
+            }
+
+            public  DataTable ReadData(SQLiteConnection conn)
+            {
+                DataTable dt = new DataTable();
+                SQLiteDataReader SQLread;
+                SQLiteCommand SQLcmd = conn.CreateCommand();
+                SQLcmd.CommandText = "SELECT * FROM Data ORDER BY TIME DESC limit 20;";
+                SQLread = SQLcmd.ExecuteReader();
+                dt.Load(SQLread);
+                Console.WriteLine(dt);
+                return dt;
+            }
+        }
+    }
